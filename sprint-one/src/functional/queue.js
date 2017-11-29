210c7da3173b7f61139e9401b687b3f37401151a
Your Name <williamputnamjr@gmail.com>
2017-11-29 12:27:57 -0800

    attempint functional with shared methods for stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage.counter = value;
+    counter++;
+    return value;
   };
 
   someInstance.dequeue = function() {
+    counter--;
+    return storage[0];
+    delete storage[0];
+
   };
 
   someInstance.size = function() {
+    if (counter < 0 || counter === undefined) {
+      return 0;
+    }
+    return counter;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,36 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage.count = value;
+    count++;
+    return value;
   };
 
   someInstance.pop = function() {
-  };
+    count--;
+  
+    //for (var key in storage) {
+    return storage[count+1];
+    delete storage[count+1];
+      //if (key === Object.keys(storage).length) {
+       // someInstance[key] = storage[key];
+       // return storage[key];
+        //delete key;
+      //}
+    //}
+  };//
 
   someInstance.size = function() {
+    if (count < 0 || count === undefined) {
+      return 0;
+    }
+    return count;
   };
+
 
   return someInstance;
 };
