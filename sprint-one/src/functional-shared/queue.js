73c555bad72bdcec350f8b03dff219c8d7289334
wsputnam <williamputnamjr@gmail.com>
2017-11-29 16:08:14 -0800

    progress made on functional shared queue + stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,48 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+
+  'storage': {},
+  'counter': 0,
+
+  'enqueue': function(value) {
+    this.counter++;
+    this.storage[this.counter] = value;
+    return value;
+  },
+  
+  'dequeue': function() {
+    
+    var popped = this.storage[0];
+  
+    
+    for (var i = 0; i <= this.counter; i++) {
+      this.storage[i] = this.storage[i + 1];
+    }
+    
+    this.counter--;
+    
+    return popped;
+    
+  },
+  
+  'size': function() {
+    if (this.counter < 0 || this.counter === undefined) {
+      return 0;
+    }
+    
+    return this.counter;
+    
+  }
+
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,42 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  
+  'storage': {},
+  'counter': 0,
+
+  'push': function(value) {
+    this.counter++;
+    this.storage[this.counter] = value;
+    return value;
+  },
+  
+  'pop': function() {
+    
+    var popped = this.storage[this.counter];
+    delete this.storage[this.counter];
+
+    this.counter--;
+    return popped;
+    
+  },
+  
+  'size': function() {
+    if (this.counter < 0 || this.counter === undefined) {
+      return 0;
+    }
+    
+    return this.counter;
+    
+  }
+};
 
 
