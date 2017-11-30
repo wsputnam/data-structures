6c8d244ef257e64017b4b808c9b3013b99e0c2ac
wsputnam <williamputnamjr@gmail.com>
2017-11-29 17:32:55 -0800

    implement all prototype functions

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.counter = 0;
+  return someInstance;
 };
 
-var stackMethods = {};
+stackMethods = {};
+
+stackMethods.push = function(value) { 
+  this.counter++;
+  this.storage[this.counter] = value;
+  
+  return value;
+};
+
+stackMethods.pop = function() {
+  var popped = this.storage[this.counter];
+  
+  
+  this.counter--;
+  
+  return popped;
+  
+};
+
+stackMethods.size = function() {
+  if (this.counter < 0 || this.counter === undefined) {
+    return 0;
+  }
+  return this.counter;
+};
 
 
