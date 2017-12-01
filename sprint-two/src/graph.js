8b2ef3e391735307c3ba23c439a1be639dfcd21b
wsputnam <williamputnamjr@gmail.com>
2017-12-01 15:52:21 -0800

    done with graphs :)

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -3,7 +3,6 @@
 // Instantiate a new graph
 var Graph = function() {
   this.nodes = [];
-  this.index = [];
   this.edges = [];
 };
 
@@ -12,51 +11,53 @@ var Graph = function() {
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
   //subarray is for edges
-  var graphPoint = node;
-  this.nodes.push(graphPoint);
-  this.index.push(node);
+  this.nodes.push(node);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
   // 
-  if (this.nodes[0]) { 
-    return this.nodes[0].indexOf(node) !== -1;  
-  }
-  return false;
+  return this.nodes.indexOf(node) !== -1;
+  
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  var nodeLoc = this.nodes[0].indexOf(node);
+  var spliceFunc = this.edges.splice.bind(this.edges);
+  var nodeLoc = this.nodes.indexOf(node);
   nodeLoc !== -1 && this.nodes.splice(nodeLoc, 1);
+  this.edges.forEach(function(edge, i) {
+    if (JSON.parse(edge)[0] === node || JSON.parse(edge)[1] === node) {
+      spliceFunc(i, 1);
+    }
+  });
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-  var firstNode = this.index.indexOf(fromNode);
-  var secondNode = this.index.indexOf(toNode);
-  if (firstNode !== -1 && secondNode !== -1) {
-    if (this.nodes[firstNode][1].indexOf(toNode) !== -1 && this.nodes[secondNode][1].indexOf(fromNode) !== -1) {
-      return true;
-    } 
-  }
-  return false;
-  
+  return this.edges.indexOf(JSON.stringify([fromNode, toNode])) !== -1 || this.edges.indexOf(JSON.stringify([toNode, fromNode])) !== -1;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  // var firstNode = this.index.indexOf(fromNode);
-  // var secondNode = this.index.indexOf(toNode);
-  // this.nodes[firstNode][1].concat(toNode);
-  // this.nodes[secondNode][1].concat(fromNode);
-  this.edges.push([fromNode, toNode]);
+  // var firstNode = this.nodes.indexOf(fromNode);
+  // fromNode.push(edges);
+  // toNode[1].push(fromNode);
+  if (this.nodes.indexOf(fromNode) !== -1 && this.nodes.indexOf(toNode) !== -1) {
+    this.edges.push(JSON.stringify([fromNode, toNode])); 
+  }
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-  
+  var foundEdge = this.edges.indexOf(JSON.stringify([fromNode, toNode]));
+  var foundEdge2 = this.edges.indexOf(JSON.stringify([toNode, fromNode]));
+  if (foundEdge !== -1) {
+    this.edges.splice(foundEdge, 1);
+  } 
+  if (foundEdge2 !== -1) {
+    this.edges.splice(foundEdge2, 1);
+  }
 };
 
 // Pass in a callback which will be executed on each node of the graph.
@@ -66,15 +67,10 @@ Graph.prototype.forEachNode = function(cb) {
   });
 };
 
-// var Node = function(value) {
-
-//   this.value = value;
-//   this.edges = []; // store connected dges here
-
-// };
-
 /*
  * Complexity: What is the time complexity of the above functions?
+  addNode is constant
+  The rest are linear
  */
 
 
